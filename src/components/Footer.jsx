78aa5e4f5f466e4565820c5fcbb2b0e6007e944b
Marianne <mariannemukai@gmail.com>
2026-04-27 11:36:29 -0300

    fix: ajustando erro de identação

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@ import '../css/card.css'
 
 const Footer = () => {
   return (
-   <section>
-    <h6>Copyright © 2026 - GameStore</h6>
-   </section>
+    <section>
+      <h6>Copyright © 2026 - GameStore</h6>
+    </section>
   )
 }
 

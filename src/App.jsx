e873d535108dcf82870e9492df89e2b7cc7e30bd
maria <mariadruzeic.06@gmail.com>
2026-04-27 21:43:40 -0300

    fix: arrumando footer e header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+
 import Home from "./routes/Home"
 import Produto from "./routes/Produto"
 import Error from "./routes/Error"
@@ -7,11 +10,15 @@ import Error from "./routes/Error"
 function App() {
   return (
     <BrowserRouter>
+      <Header />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/produtos" element={<Produto />} />
         <Route path="*" element={<Error />} />
       </Routes>
+
+      <Footer />
     </BrowserRouter>
   )
 }
